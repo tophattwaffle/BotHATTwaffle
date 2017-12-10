0ec147419d0a58a325144a33752feb8f63d7bad0
Benjamin Thomas Blodgett <benvlodgi@gmail.com>
2017-12-10 16:23:43 -0500

    Add Extensions class

---
+++ BotHATTwaffle/Extensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BotHATTwaffle
+{
+    /// <summary> Static Extensions class used for adding methods to existing libraries and classes </summary>
+    public static class Extensions
+    {
+    }
+}
