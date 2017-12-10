4657917917e73d73834b3efcc5f1a6647531793e
Benjamin Thomas Blodgett <benvlodgi@gmail.com>
2017-12-10 16:24:29 -0500

    Add AddKeyIfMissing static extension function to simplify default value management

--- BotHATTwaffle/Extensions.cs
+++ BotHATTwaffle/Extensions.cs
@@ -9,5 +9,21 @@ namespace BotHATTwaffle
     /// <summary> Static Extensions class used for adding methods to existing libraries and classes </summary>
     public static class Extensions
     {
+        /// <summary> Adds given key and value to dictionary if the given key does not already exist in dictionary.</summary>
+        /// <typeparam name="TKey">Dictionary Key Type.</typeparam>
+        /// <typeparam name="TValue">Dictionary Value Type.</typeparam>
+        /// <param name="dictionary"></param>
+        /// <param name="key">Key to check if it exists.</param>
+        /// <param name="valueIfMissing">Value to add if the key was missing.</param>
+        /// <returns>True if key was found. False if missing value was used.</returns>
+        public static bool AddKeyIfMissing<TKey, TValue>(this Dictionary<TKey, TValue> dictionary, TKey key, TValue valueIfMissing)
+        {
+            if (!dictionary.ContainsKey(key))
+            {
+                dictionary.Add(key, valueIfMissing);
+                return false;
+            }
+            return true;
+        }
     }
 }
