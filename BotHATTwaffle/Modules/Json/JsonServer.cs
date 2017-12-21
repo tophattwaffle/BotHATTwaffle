f04176a0033410bb2415620098102e98111c7f86
TopHATTwaffle <tophattwaffle@gmail.com>
2017-12-21 04:44:35 -0600

    Added variables to handle FTP/FTPS/SFTP server settings.

--- BotHATTwaffle/Modules/Json/JsonServer.cs
+++ BotHATTwaffle/Modules/Json/JsonServer.cs
@@ -12,5 +12,13 @@ namespace BotHATTwaffle.Modules.Json
         public string Address { get; set; }
         [JsonProperty("password")]
         public string Password { get; set; }
+        [JsonProperty("ftppath")]
+        public string FTPPath { get; set; }
+        [JsonProperty("ftpuser")]
+        public string FTPUser { get; set; }
+        [JsonProperty("ftppass")]
+        public string FTPPass { get; set; }
+        [JsonProperty("ftptype")]
+        public string FTPType { get; set; }
     }
 }
