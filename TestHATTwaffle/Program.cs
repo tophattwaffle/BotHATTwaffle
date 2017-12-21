674cde19350d7c3e598a77010432c63d15b45600
TopHATTwaffle <tophattwaffle@gmail.com>
2017-12-21 04:43:29 -0600

    Misc Testing changes

--- TestHATTwaffle/Program.cs
+++ TestHATTwaffle/Program.cs
@@ -3,10 +3,15 @@
  */
 using CoreRCON;
 using CoreRCON.Parsers.Standard;
+using FluentFTP;
 using System;
 using System.Net;
+using System.Security.Authentication;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.IO;
+using Renci.SshNet;
+using Renci.SshNet.Sftp;
 
 namespace TestHATTwaffle
 {
@@ -14,8 +19,15 @@ namespace TestHATTwaffle
     {
         private static void Main(string[] args)
         {
-            string externalip = new WebClient().DownloadString("http://icanhazip.com");
-            Console.WriteLine(externalip);
+            string user = "TopHATTwaffle#1679";
+
+            var split = user.Split('#');
+
+            Console.WriteLine(split[0]);
+            Console.WriteLine(split[1]);
+
+            Console.ReadLine();
         }
+
     }
 }
