6b42b7fc96a671e71e1e06e6a5b588eb47ef5c2e
Douglas Hoogland <tophattwaffle@gmail.com>
2018-01-01 22:36:43 -0600

    Moved into objects folder. Added varaibles and method for handling server reservations.

---
+++ BotHATTwaffle/Objects/UserData.cs
@@ -0,0 +1,36 @@
+﻿using BotHATTwaffle.Modules.Json;
+using Discord;
+using Discord.WebSocket;
+using System;
+
+namespace BotHATTwaffle
+{
+    public class UserData
+    {
+        public SocketGuildUser User { get; set; }
+        public DateTime UnmuteTime { get; set; }
+        public DateTime ServerReleaseTime { get; set; }
+        public DateTime JoinRoleTime { get; set; }
+        public Embed JoinMessage { get; set; }
+        public JsonServer Server { get; set; }
+
+        public UserData()
+        {
+        }
+
+        public bool CanUnmute()
+        {
+            return UnmuteTime.CompareTo(DateTime.Now) < 0;
+        }
+
+        public bool CanReleaseServer()
+        {
+            return ServerReleaseTime.CompareTo(DateTime.Now) < 0;
+        }
+
+        public bool CanRole()
+        {
+            return JoinRoleTime.CompareTo(DateTime.Now) < 0;
+        }
+    }
+}
