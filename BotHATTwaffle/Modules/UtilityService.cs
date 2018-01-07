630a885a0b1985f965a0c7e104b5996e8e391246
TopHATTwaffle <tophattwaffle@gmail.com>
2018-01-07 16:09:22 -0600

    >role me can now accept multiple roles at once. No longer case sensitive.

--- BotHATTwaffle/Modules/UtilityService.cs
+++ BotHATTwaffle/Modules/UtilityService.cs
@@ -50,11 +50,11 @@ namespace BotHATTwaffle.Modules
         }
 
         [Command("roleme")]
-        [Summary("`>roleme [rolename]` Toggles roles on a user")]
+        [Summary("`>roleme [role names]` Toggles roles on a user")]
         [Remarks("This will let you add roles to yourself. Typically for saying you have a skill like 3D Modeling, or level design." +
-            "\n__Channel names are case sensitive!!!__\n" +
-            "You can type `>roleme` to show all roles available")]
-        public async Task RolemeAsync([Remainder]string inRoleStr = null)
+            "\nYou can put multiple roles into one command to get multiple at one time. Example: `>roleme blender level design programmer`" +
+            "\nYou can type `>roleme` to show all roles available")]
+        public async Task RolemeAsync([Remainder]string inRoleStr = "display")
         {
             if (Context.IsPrivate)
             {
@@ -63,45 +63,47 @@ namespace BotHATTwaffle.Modules
             }
 
             var user = Context.User as SocketGuildUser;
-            var inRole = Context.Guild.Roles.FirstOrDefault(x => x.Name == inRoleStr);
 
             //Display roles, or modify role state
-            if (inRoleStr == null)
+            if (inRoleStr.ToLower() == "display")
             {
-                await ReplyAsync($"*Remember roles are case sensitive.* Valid roles are:```\n{string.Join("\n", _dataServices.roleMeWhiteList)}```");
+                await ReplyAsync($"Valid roles are:```\n{string.Join("\n", _dataServices.roleMeWhiteList)}```");
             }
             else
             {
                 //Validate that we can apply the role
                 Boolean valid = false;
+                string reply = null;
                 foreach (string s in _dataServices.roleMeWhiteList)
                 {
-                    if (inRoleStr.Contains(s))
+                    if (inRoleStr.ToLower().Contains(s.ToLower()))
                     {
-                        valid = true;
-                        break;
+                        valid = true; //We applied at least 1 role.
+
+                        var inRole = Context.Guild.Roles.FirstOrDefault(x => x.Name == s);
+
+                        if (user.Roles.Contains(inRole))
+                        {
+                            reply += $"{Context.User.Username} lost the **{inRole}** role.\n";
+                            await (user as IGuildUser).RemoveRoleAsync(inRole);
+                        }
+                        else
+                        {
+                            reply += $"{Context.User.Username} now has the role **{inRole}**. Enjoy the flair!\n";
+                            await (user as IGuildUser).AddRoleAsync(inRole);
+                        }
                     }
                 }
 
-                if (valid)
+                if(valid)
                 {
-                    if (user.Roles.Contains(inRole))
-                    {
-                        await _dataServices.ChannelLog($"{Context.User} has removed {inRoleStr} role from themselves.");
-                        await ReplyAsync($"{Context.User.Mention} lost the **{inRoleStr}** role.");
-                        await (user as IGuildUser).RemoveRoleAsync(inRole);
-                    }
-                    else
-                    {
-                        await _dataServices.ChannelLog($"{Context.User} has assigned themselves the role of {inRoleStr}");
-                        await ReplyAsync($"{Context.User.Mention} now has the role **{inRoleStr}**. Enjoy the flair!");
-                        await (user as IGuildUser).AddRoleAsync(inRole);
-                    }
+                    await ReplyAsync($"{reply}");
+                    await _dataServices.ChannelLog($"{Context.User}\n{reply}");
                 }
                 else
                 {
-                    await ReplyAsync($"{Context.User.Mention} you cannot assign yourself the role of **{inRoleStr}** because it does not exist, " +
-                        $"or it is not allowed.");
+                    await ReplyAsync($"{Context.User.Mention}\n```You cannot assign yourself the role of **{inRoleStr}** because it does not exist, " +
+                        $"or it is not allowed.```");
                     await _dataServices.ChannelLog($"{Context.User} attempted to roleMe the role of: {inRoleStr} and it failed. Either due to the " +
                         $"role not existing, or they tried to use a role that isn't in the white list.");
                 }
