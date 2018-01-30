0e7eeec2ea5b8e14889c0350c25f939edbe0f75f
MarkKoz <KozlovMark@gmail.com>
2018-01-30 06:33:54 -0800

    Refactored TimerService (#8).
    * Removed useless TimerModule.
    * Changed Timer into a private readonly field; it was unused elsewhere.

--- BotHATTwaffle/Modules/TimerService.cs
+++ BotHATTwaffle/Modules/TimerService.cs
@@ -1,79 +1,57 @@
-﻿#pragma warning disable CS4014 //Disable the async warning. #yolo
+﻿#pragma warning disable CS4014 // Disables the unawaited warning. #yolo
+
 using System;
 using System.Threading;
-using Discord.Commands;
+
 using Discord.WebSocket;
 
 namespace BotHATTwaffle.Modules
 {
 	public class TimerService
 	{
-
-		public Timer Timer;
-		private readonly LevelTesting _levelTesting;
-		private readonly ModerationServices _mod;
 		private readonly DiscordSocketClient _client;
-		private readonly Random _random;
 		private readonly DataServices _dataServices;
+		private readonly Random _random;
+		private readonly Timer _timer;
 
-		public TimerService(DiscordSocketClient client, ModerationServices mod, LevelTesting levelTesting, Random rand, DataServices dataServices)
+		public TimerService(
+			DiscordSocketClient client,
+			DataServices dataServices,
+			LevelTesting levelTesting,
+			ModerationServices mod,
+			Random rand)
 		{
-			_dataServices = dataServices;
 			_client = client;
-			_mod = mod;
-			_levelTesting = levelTesting;
+			_dataServices = dataServices;
 			_random = rand;
 
-			//Code inside this will fire ever {updateInterval} seconds
-			Timer = new Timer(_ =>
+			// Code inside this will fire every {updateInterval} seconds.
+			_timer = new Timer(_ =>
 			{
-				_levelTesting.Announce();
-				_levelTesting.CheckServerReservations();
-				_mod.Cycle();
+				levelTesting.Announce();
+				levelTesting.CheckServerReservations();
+				mod.Cycle();
 				ChangePlaying();
 			},
 			null,
-			TimeSpan.FromSeconds(_dataServices.StartDelay),  // Time that message should fire after bot has started
-			TimeSpan.FromSeconds(_dataServices.UpdateInterval)); // Time after which message should repeat (`Timeout.Infinite` for no repeat)
+			TimeSpan.FromSeconds(_dataServices.StartDelay), // Time that message should fire after bot has started.
+			TimeSpan.FromSeconds(_dataServices.UpdateInterval)); // Time after which message should repeat (Timeout.Infinite for no repeat).
 		}
 
 		public void Stop()
 		{
-			Timer.Change(Timeout.Infinite, Timeout.Infinite);
-			Console.WriteLine($"Timer stopped!\n");
+			_timer.Change(Timeout.Infinite, Timeout.Infinite);
+			Console.WriteLine("Timer stopped!\n");
 		}
 
 		public void Restart()
 		{
-			Timer.Change(TimeSpan.FromSeconds(_dataServices.StartDelay), TimeSpan.FromSeconds(_dataServices.UpdateInterval));
-			Console.WriteLine($"Timer restarted" +
-							  $"\nStart Delay: {_dataServices.StartDelay}" +
-							  $"\nUpdate Interval {_dataServices.UpdateInterval}\n");
+			_timer.Change(TimeSpan.FromSeconds(_dataServices.StartDelay), TimeSpan.FromSeconds(_dataServices.UpdateInterval));
+			Console.WriteLine(
+				$"Timer restarted.\nStart Delay: {_dataServices.StartDelay}\nUpdate Interval: {_dataServices.UpdateInterval}\n");
 		}
 
-		public void ChangePlaying() => _client.SetGameAsync(_dataServices.PlayingStrings[_random.Next(0, _dataServices.PlayingStrings.Length)]);
-	}
-
-	public class TimerModule : ModuleBase
-	{
-		private readonly TimerService _service;
-
-		public TimerModule(TimerService service)
-		{
-			_service = service;
-		}/*
-	[Command("stoptimer")]
-	public async Task StopCmd()
-	{
-		_service.Stop();
-		await ReplyAsync("Timer stopped.");
-	}
-
-	[Command("starttimer")]
-	public async Task RestartCmd()
-	{
-		_service.Restart();
-		await ReplyAsync("Timer (re)started.");
-	}*/
+		public void ChangePlaying() =>
+			_client.SetGameAsync(_dataServices.PlayingStrings[_random.Next(0, _dataServices.PlayingStrings.Length)]);
 	}
 }
