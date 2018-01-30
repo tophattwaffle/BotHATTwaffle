e8c4d1ae7af408b4b4e6bc713eed9f4cd9056ce4
MarkKoz <KozlovMark@gmail.com>
2018-01-30 06:37:30 -0800

    Refactored ShadersModule (#8, #9).
    * Revised some field titles and values.

--- BotHATTwaffle/Modules/Shaders.cs
+++ BotHATTwaffle/Modules/Shaders.cs
@@ -1,61 +1,59 @@
-﻿using Discord;
-using Discord.Commands;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Net;
-using System.Text.RegularExpressions;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
+
+using Discord;
+using Discord.Commands;
 
 namespace BotHATTwaffle.Modules
 {
-
+	/// <summary>
+	/// Contains commands related to Source shader development.
+	/// </summary>
 	public class ShadersModule : ModuleBase<SocketCommandContext>
 	{
-
 		[Command("shader")]
-		[Summary("`>shader` Gives you a shader information")]
+		[Summary("`>shader` Provides links to Source shader development resources.")]
 		[Alias("shaders")]
 		public async Task ShaderAsync()
 		{
-			List<EmbedFieldBuilder> fieldBuilder = new List<EmbedFieldBuilder>();
-
-			var authBuilder = new EmbedAuthorBuilder()
+			var embed = new EmbedBuilder
 			{
-				Name = "Shader Resources",
-				IconUrl = "https://cdn.discordapp.com/icons/111951182947258368/0e82dec99052c22abfbe989ece074cf5.png",
-			};
-
-			fieldBuilder.Add(new EmbedFieldBuilder{
-				Name = "General Shader",
-				Value = $"[General Shader](https://developer.valvesoftware.com/wiki/Shader)" +
-						$"\n[Shader Authoring](https://developer.valvesoftware.com/wiki/Shader_Authoring)" +
-						$"\n[Guide to Shaders](http://www.bit-tech.net/reviews/tech/guide_to_shaders/1/)" +
-						$"\n[Introduction to Shaders](http://www.moddb.com/games/half-life-2/tutorials/introduction-to-shaders)" +
-						$"\n[Post Process Shaders](http://www.moddb.com/games/half-life-2/tutorials/post-process-shader)", IsInline = false });
-			fieldBuilder.Add(new EmbedFieldBuilder
-			{
-				Name = "Shader Language",
-				Value = $"[High-Level Shader Language](https://en.wikipedia.org/wiki/High-Level_Shading_Language)",
-				IsInline = false
-			});
-			fieldBuilder.Add(new EmbedFieldBuilder
-			{
-				Name = "Shader Types",
-				Value = $"[Vertex](https://developer.valvesoftware.com/wiki/Shader#Vertex_shaders )" +
-						$"\n[Pixel](https://developer.valvesoftware.com/wiki/Shader#Pixel_shaders)",
-				IsInline = false
-			});
-
-			var builder = new EmbedBuilder()
-			{
-				Author = authBuilder,
-				Fields = fieldBuilder,
+				Author = new EmbedAuthorBuilder
+				{
+					Name = "Shader Resources",
+					IconUrl = "https://cdn.discordapp.com/icons/111951182947258368/0e82dec99052c22abfbe989ece074cf5.png"
+				},
+				Fields = new List<EmbedFieldBuilder>
+				{
+					new EmbedFieldBuilder
+					{
+						Name = "General",
+						Value = "[Shader](https://developer.valvesoftware.com/wiki/Shader)\n" +
+						        "[Shader Authoring](https://developer.valvesoftware.com/wiki/Shader_Authoring)\n" +
+						        "[Guide to Shaders](http://www.bit-tech.net/reviews/tech/guide_to_shaders/1/)\n" +
+						        "[Introduction to Shaders](http://www.moddb.com/games/half-life-2/tutorials/introduction-to-shaders)\n" +
+						        "[Post Process Shaders](http://www.moddb.com/games/half-life-2/tutorials/post-process-shader)",
+						IsInline = false
+					},
+					new EmbedFieldBuilder
+					{
+						Name = "Language",
+						Value = "[High-Level Shader Language](https://en.wikipedia.org/wiki/High-Level_Shading_Language)",
+						IsInline = false
+					},
+					new EmbedFieldBuilder
+					{
+						Name = "Types",
+						Value = "[Vertex](https://developer.valvesoftware.com/wiki/Shader#Vertex_shaders)\n" +
+						        "[Pixel](https://developer.valvesoftware.com/wiki/Shader#Pixel_shaders)",
+						IsInline = false
+					}
+				},
 				Color = new Color(240, 235, 230),
-				Description = "Here are a list of popular shader resources."
-
+				Description = "Here are lists of popular shader resources:"
 			};
-			await ReplyAsync("", false, builder.Build());
+
+			await ReplyAsync(string.Empty, false, embed.Build());
 		}
 	}
 }
