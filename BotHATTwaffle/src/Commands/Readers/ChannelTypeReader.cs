ebd0e0ae629eb4b8bca2d9421bb0302d756e07c8
MarkKoz <KozlovMark@gmail.com>
2018-03-31 19:22:51 -0700

    Added direct support for IGuild in ChannelTypeReader.

--- BotHATTwaffle/src/Commands/Readers/ChannelTypeReader.cs
+++ BotHATTwaffle/src/Commands/Readers/ChannelTypeReader.cs
@@ -23,24 +23,24 @@ namespace BotHATTwaffle.Commands.Readers
         /// See flow chart by Still#2876:
         /// https://cdn.discordapp.com/attachments/381889909113225237/409209957683036160/ChannelTypeReader.png
         /// </remarks>
-        /// <param name="context">The context in which to search for the channel.</param>
+        /// <param name="guild">The guild in which to search for the channel.</param>
         /// <param name="input">A string representing a channel by mention, id, or name.</param>
         /// <returns>The results of the parse.</returns>
-        public static async Task<TypeReaderResult> ReadAsync(ICommandContext context, string input)
+        public static async Task<TypeReaderResult> ReadAsync(IGuild guild, string input)
         {
-            if (context.Guild != null)
+            if (guild != null)
             {
                 var results = new Dictionary<ulong, TypeReaderValue>();
-                var channels = await context.Guild.GetChannelsAsync(CacheMode.CacheOnly).ConfigureAwait(false);
+                var channels = await guild.GetChannelsAsync(CacheMode.CacheOnly).ConfigureAwait(false);
                 ulong id;
 
                 // By Mention (1.0)
                 if (MentionUtils.TryParseChannel(input, out id))
-                    AddResult(results, await context.Guild.GetChannelAsync(id, CacheMode.CacheOnly).ConfigureAwait(false) as T, 1.00f);
+                    AddResult(results, await guild.GetChannelAsync(id, CacheMode.CacheOnly).ConfigureAwait(false) as T, 1.00f);
 
                 // By Id (0.9)
                 if (ulong.TryParse(input, NumberStyles.None, CultureInfo.InvariantCulture, out id))
-                    AddResult(results, await context.Guild.GetChannelAsync(id, CacheMode.CacheOnly).ConfigureAwait(false) as T, 0.90f);
+                    AddResult(results, await guild.GetChannelAsync(id, CacheMode.CacheOnly).ConfigureAwait(false) as T, 0.90f);
 
                 // By Name (0.7-0.8)
                 // Acounts for name being null because GetChannelsAsync returns categories in 1.0.
@@ -57,15 +57,24 @@ namespace BotHATTwaffle.Commands.Readers
         /// <summary>
         /// Gets the best channel result for the given string.
         /// </summary>
-        /// <param name="context">The context in which to search for the channel.</param>
+        /// <param name="guild">The guild in which to search for the channel.</param>
         /// <param name="input">A string representing a channel by mention, id, or name.</param>
         /// <returns>The channel result with the highest score or <c>null</c> if no results exist.</returns>
-        public static async Task<T> GetBestResultAsync(ICommandContext context, string input)
+        public static async Task<T> GetBestResultAsync(IGuild guild, string input)
         {
-            TypeReaderResult result = await ReadAsync(context, input);
+            TypeReaderResult result = await ReadAsync(guild, input);
 
             return GetBestResult(result);
         }
+
+        /// <summary>
+        /// Gets the best channel result for the given string.
+        /// </summary>
+        /// <param name="context">The context in which to search for the channel.</param>
+        /// <param name="input">A string representing a channel by mention, id, or name.</param>
+        /// <returns>The channel result with the highest score or <c>null</c> if no results exist.</returns>
+        public static async Task<T> GetBestResultAsync(ICommandContext context, string input) =>
+            await GetBestResultAsync(context.Guild, input);
 
         /// <summary>
         /// Gets the best channel result from the given results.
@@ -86,7 +95,7 @@ namespace BotHATTwaffle.Commands.Readers
         /// <param name="results">The dictionary to which to add the result.</param>
         /// <param name="channel">The result's channel.</param>
         /// <param name="score">The result's score.</param>
-        private static void AddResult(Dictionary<ulong, TypeReaderValue> results, T channel, float score)
+        private static void AddResult(IDictionary<ulong, TypeReaderValue> results, T channel, float score)
         {
             if (channel != null && !results.ContainsKey(channel.Id))
                 results.Add(channel.Id, new TypeReaderValue(channel, score));
